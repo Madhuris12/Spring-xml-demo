00216439b82e6044d9b5e21375037f5a6e848416
madhuri <Madhuris12>
2019-05-14 22:42:26 +0530

    Initial commit

---
+++ src/main/java/com/stackroute/spring/Main.java
@@ -0,0 +1,21 @@
+package com.stackroute.spring;
+
+import com.stackroute.spring.beans.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+public class Main {
+        public static void main(String[] args) {
+            ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+            Movie beanA = (Movie) context.getBean("MovieA");
+            Movie beanB = (Movie) context.getBean("MovieB");
+            Movie beanC = (Movie) context.getBean("MovieC");
+            System.out.println(beanA.toString());
+            System.out.println(beanB.toString());
+            System.out.println(beanC.toString());
+
+            System.out.println(beanA==beanB);
+        }
+}
---
+++ src/main/java/com/stackroute/spring/beans/Movie.java
@@ -0,0 +1,28 @@
+package com.stackroute.spring.beans;
+
+public class Movie {
+    Actor actor;
+
+    Movie() {
+
+    }
+
+    Movie(Actor actor) {
+        this.actor = actor;
+    }
+
+    public Actor getActor() {
+        return actor;
+    }
+
+    public void setActor(Actor actor) {
+        this.actor = actor;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "actor=" + actor +
+                '}';
+    }
+}

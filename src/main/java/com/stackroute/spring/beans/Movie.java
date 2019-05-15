04cd25b957032a23afc7d4f92a64be1b90614af8
Madhuris12 <49709761+Madhuris12@users.noreply.github.com>
2019-05-15 10:29:55 +0530

    Update Movie.java

--- src/main/java/com/stackroute/spring/beans/Movie.java
+++ src/main/java/com/stackroute/spring/beans/Movie.java
@@ -1,28 +1,52 @@
 package com.stackroute.spring.beans;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import com.stackroute.spring.*;
+
 public class Movie {
-    Actor actor;
 
-    Movie() {
+        private String movieName;
+        private String director;
+        private Actor actor;
 
+        public Movie() { }
+        @Autowired
+        public Movie(String movieName, String director, Actor actor1) {
+            this.movieName = movieName;
+            this.director = director;
+            this.actor= actor1;
+        }
+
+        @Override
+        public String toString() {
+            return "Movie{" +
+                    "movieName='" + movieName + '\'' +
+                    ", director='" + director + '\'' +
+                    ", actor=" + actor +
+                    '}';
+        }
+
+        public String getMovieName() {
+            return movieName;
+        }
+
+        public void setMovieName(String movieName) {
+            this.movieName = movieName;
+        }
+
+        public String getDirector() {
+            return director;
+        }
+
+        public void setDirector(String director) {
+            this.director = director;
+        }
+
+        public Actor getActor() {
+            return actor;
+        }
+
+        public void setActor(Actor actor) {
+            this.actor = actor;
+        }
     }
-
-    Movie(Actor actor) {
-        this.actor = actor;
-    }
-
-    public Actor getActor() {
-        return actor;
-    }
-
-    public void setActor(Actor actor) {
-        this.actor = actor;
-    }
-
-    @Override
-    public String toString() {
-        return "Movie{" +
-                "actor=" + actor +
-                '}';
-    }
-}
